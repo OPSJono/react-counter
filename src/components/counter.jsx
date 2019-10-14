e0f6ed675990f97134956f288abd4dc35b5c8993
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2019-10-14 20:04:04 +0100

    Got items incrementing and decrementing individually and keeping track
    of their own counts.
    Having their own +/- buttons enabled and disabled as they go.
    Formatting the display of each counter still works.
    
    Next: ability to remove an item from the list.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,85 +3,123 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["orange", "apple", "banana", "pear"],
-    decrementClasses: ""
+    items: [
+      { id: 1, name: "Orange", count: 0 },
+      { id: 2, name: "Apple", count: 0 },
+      { id: 3, name: "Banana", count: 0 },
+      { id: 4, name: "Pear", count: 0 }
+    ]
   };
 
   //   constructor() {
   //     super();
   //   }
 
-  formatCount() {
-    let { count } = this.state;
-    if (count === 0) {
-      count = "Zero";
-    }
+  formatCount = product => {
+    let { items } = this.state;
+
+    let count = "Zero";
+
+    items.forEach(function(item) {
+      if (item.id === product) {
+        if (item.count > 0) {
+          count = item.count;
+        }
+      }
+    });
 
     return count;
-  }
+  };
 
-  getBadgeClasses() {
-    let { count } = this.state;
+  getBadgeClasses = product => {
+    let { items } = this.state;
 
     let classes = "m-2 badge badge-";
 
-    if (count === 0) {
-      classes += "warning";
-    } else {
-      classes += "primary";
-    }
+    items.forEach(function(item) {
+      if (item.id === product) {
+        if (item.count === 0) {
+          classes += "warning";
+        } else {
+          classes += "primary";
+        }
+      }
+    });
 
     return classes;
-  }
+  };
 
-  getDecrementClasses() {
+  getDecrementClasses = product => {
     let classes = "btn btn-secondary btn-sm";
+    const { items } = this.state;
 
-    if (this.state.count === 0) {
-      classes += " disabled";
-    }
+    items.forEach(function(item) {
+      if (item.id === product) {
+        if (item.count === 0) {
+          classes += " disabled";
+        }
+      }
+    });
 
     return classes;
-  }
+  };
 
-  renderTags() {
-    const { tags } = this.state;
+  renderItems() {
+    const { items } = this.state;
 
-    if (tags.length === 0) {
+    if (items.length === 0) {
       return <p>There are no items in this list.</p>;
     }
 
     return (
       <ul>
-        {tags.map(tag => (
-          <li key={tag}>{tag}</li>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name}
+            <span className={this.getBadgeClasses(item.id)}>{this.formatCount(item.id)}</span>
+            <button onClick={() => this.handleIncrement(item.id)} className="btn m-2 btn-secondary btn-sm">
+              Increment
+            </button>
+            <button onClick={() => this.handleDecrement(item.id)} className={this.getDecrementClasses(item.id)}>
+              Decrement
+            </button>
+          </li>
         ))}
       </ul>
     );
   }
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = product => {
+    let { items } = this.state;
+
+    items.forEach(function(item) {
+      if (item.id === product) {
+        item.count = item.count + 1;
+      }
+    });
+
+    this.setState({ items: items });
   };
 
-  handleDecrement = () => {
-    const { count } = this.state;
-    if (count > 0) {
-      this.setState({ count: count - 1 });
-    }
+  handleDecrement = product => {
+    let { items } = this.state;
+
+    items.forEach(function(item) {
+      if (item.id === product) {
+        if (item.count > 0) {
+          item.count = item.count - 1;
+        }
+      }
+    });
+
+    this.setState({ items: items });
   };
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn m-2 btn-secondary btn-sm">
-          Increment
-        </button>
-        <button onClick={this.handleDecrement} className={this.getDecrementClasses()}>
-          Decrement
-        </button>
-        <ul>{this.renderTags()}</ul>
+        <div className="card card-header">Shopping Cart</div>
+        <div className="card card-body">{this.renderItems()}</div>
       </React.Fragment>
     );
   }
